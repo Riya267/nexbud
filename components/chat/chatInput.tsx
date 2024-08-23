6f8f15c50f202742150c9aa4a413d546cc7d68da
Riya267 <riyacec05@gmail.com>
2024-08-23 23:26:43 +0530

    minor fix added

--- components/chat/chatInput.tsx
+++ components/chat/chatInput.tsx
@@ -25,7 +25,7 @@ const ChatInput: React.FC<ChatInputProps> = ({ onSendMessage }) => {
       />
       <button
         onClick={handleSend}
-        className="mx-4 bg-gradient-to-r from-purple-500 to-pink-500 text-white px-8 rounded-lg hover:from-purple-600 hover:to-pink-600 transition"
+        className="ml-4 bg-gradient-to-r from-purple-500 to-pink-500 text-white px-8 rounded-lg hover:from-purple-600 hover:to-pink-600 transition"
       >
         Ask
       </button>
